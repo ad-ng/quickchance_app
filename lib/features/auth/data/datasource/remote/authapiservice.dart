d2ac13f2f2696a12c1d796fad6c148040aa52368
ad-ng <adolphengoga@gmail.com>
2025-04-11 12:27:59 +0200

    saving locals

--- lib/features/auth/data/datasource/remote/authapiservice.dart
+++ lib/features/auth/data/datasource/remote/authapiservice.dart
@@ -1,16 +1,25 @@
 import 'package:dio/dio.dart';
 import 'package:quickchance_app/conf/dioservice.dart';
+import 'package:quickchance_app/features/auth/data/datasource/local/tokenstore.dart';
+import 'package:quickchance_app/features/auth/data/datasource/local/userpreferences.dart';
 import 'package:quickchance_app/features/auth/data/model/login_model.dart';
 import 'package:quickchance_app/features/auth/data/model/user_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthApiService {
   final Dio _dio = DioService.instance.dio;
 
   Future<UserModel> login(LoginModel loginModel) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.clear();
     try {
       final response = await _dio.post('/auth/login', data: loginModel.toMap());
 
       final dataJson = response.data['data'];
+
+      await TokenStore.setToken(response.data['token']);
+
+      await UserPreferences().saveLocalUser(UserModel.fromJson(dataJson));
 
       return UserModel.fromMap(dataJson);
     } on DioException catch (e) {
