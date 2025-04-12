9fb24686ce6ce651202e64a486cfa5798a2d2eab
ad-ng <adolphengoga@gmail.com>
2025-04-12 21:22:05 +0200

    adding app bar

--- lib/features/home/presentation/pages/homePage.dart
+++ lib/features/home/presentation/pages/homePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quickchance_app/features/home/presentation/widgets/myappbar.dart';
 
 class Homepage extends StatefulWidget {
   const Homepage({super.key});
@@ -10,6 +11,31 @@ class Homepage extends StatefulWidget {
 class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+      child: Column(
+        children: [
+          MyAppBar(),
+          // ListView(children: [Icon(Icons.person)]),
+          SizedBox(
+            height: 100,
+            width: MediaQuery.of(context).size.width,
+            child: ListView.builder(
+              itemCount: 7,
+              scrollDirection: Axis.horizontal,
+              itemBuilder:
+                  (context, index) => Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(15),
+                      child: Image.network(
+                        'https://images.vexels.com/content/145908/preview/male-avatar-maker-2a7919.png',
+                      ),
+                    ),
+                  ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
