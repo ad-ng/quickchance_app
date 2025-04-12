ce98759e986a8e524207156e33e1a242c51625d3
ad-ng <adolphengoga@gmail.com>
2025-04-12 19:44:44 +0200

    adding bottom nav bar

--- lib/features/home/presentation/pages/landing_page.dart
+++ lib/features/home/presentation/pages/landing_page.dart
@@ -7,9 +7,32 @@ class LandingPage extends StatefulWidget {
   State<LandingPage> createState() => _LandingPageState();
 }
 
+const pages = ['HOME', 'SEARCH', 'SAVED', 'PROFILE'];
+
 class _LandingPageState extends State<LandingPage> {
+  int currentIndex = 0;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('H O M E')));
+    return Scaffold(
+      body: Center(child: Text(pages[currentIndex])),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentIndex,
+        onTap: (value) {
+          setState(() {
+            currentIndex = value;
+          });
+        },
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'home'),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'search'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.bookmarks_rounded),
+            label: 'saved',
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'profile'),
+        ],
+      ),
+    );
   }
 }
