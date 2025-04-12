343f8f16f37a72d9493ad9bf035f68a88d2c0223
ad-ng <adolphengoga@gmail.com>
2025-04-12 19:59:47 +0200

    initialising pages

--- lib/features/home/presentation/pages/landing_page.dart
+++ lib/features/home/presentation/pages/landing_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:quickchance_app/features/home/presentation/pages/homePage.dart';
+import 'package:quickchance_app/features/profile/presentation/pages/profilePage.dart';
+import 'package:quickchance_app/features/saved/presentation/pages/savedPage.dart';
+import 'package:quickchance_app/features/search/presentation/pages/searchPage.dart';
 
 class LandingPage extends StatefulWidget {
   const LandingPage({super.key});
@@ -7,14 +11,14 @@ class LandingPage extends StatefulWidget {
   State<LandingPage> createState() => _LandingPageState();
 }
 
-const pages = ['HOME', 'SEARCH', 'SAVED', 'PROFILE'];
+const pages = [Homepage(), SearchPage(), SavedPage(), ProfilePage()];
 
 class _LandingPageState extends State<LandingPage> {
   int currentIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text(pages[currentIndex])),
+      body: pages[currentIndex],
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: currentIndex,
         onTap: (value) {
