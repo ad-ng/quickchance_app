3605f4251e844d0e6fe8d300a3245d3400bedd0d
ad-ng <adolphengoga@gmail.com>
2025-04-19 09:55:28 +0200

    removing unnecessary headers

--- lib/features/home/presentation/pages/homePage.dart
+++ lib/features/home/presentation/pages/homePage.dart
@@ -14,24 +14,6 @@ class _HomepageState extends State<Homepage> {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        SizedBox(
-          height: 100,
-          width: MediaQuery.of(context).size.width,
-          child: ListView.builder(
-            itemCount: 7,
-            scrollDirection: Axis.horizontal,
-            itemBuilder:
-                (context, index) => Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(15),
-                    child: Image.network(
-                      'https://images.vexels.com/content/145908/preview/male-avatar-maker-2a7919.png',
-                    ),
-                  ),
-                ),
-          ),
-        ),
         SizedBox(height: 5),
         Wrap(
           children: [
