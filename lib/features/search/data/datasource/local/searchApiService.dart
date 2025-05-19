77d621d7671058c5d955c9447f33a7a3f4f03b53
ad-ng <adolphengoga@gmail.com>
2025-05-19 16:56:18 +0200

    fetching searches

---
+++ lib/features/search/data/datasource/local/searchApiService.dart
@@ -0,0 +1,31 @@
+import 'package:dio/dio.dart';
+import 'package:quickchance_app/conf/dioservice.dart';
+import 'package:quickchance_app/features/home/data/models/opportunity_model.dart';
+
+class SearchApiService {
+  final Dio _dio = DioService.instance.dio;
+
+  Future<List<OpportunityModel>> searchOpps(String searchQuery) async {
+    try {
+      final response = await _dio.get(
+        '/opportunity/search',
+        queryParameters: {"searchQuery": searchQuery},
+      );
+      final dataJson = response.data['data'];
+
+      if (dataJson != null && dataJson is List) {
+        return dataJson.map((json) => OpportunityModel.fromJson(json)).toList();
+      } else {
+        throw Exception(
+          'Expected a list of properties but got ${dataJson.runtimeType}',
+        );
+      }
+    } on DioException catch (e) {
+      // Handle Dio errors
+      throw e.message!;
+    } catch (e) {
+      // Catch other errors
+      return Future.error('Something went wrong: $e');
+    }
+  }
+}
