d06cad5ddceea0b7bf9d03a4c5d1989ac93441ee
ad-ng <adolphengoga@gmail.com>
2025-05-19 16:04:04 +0200

    fixing saved

--- lib/features/search/presentation/pages/searchPage.dart
+++ lib/features/search/presentation/pages/searchPage.dart
@@ -8,8 +8,47 @@ class SearchPage extends StatefulWidget {
 }
 
 class _SearchPageState extends State<SearchPage> {
+  TextEditingController searchQuery = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('S E A R C H'));
+    return Column(
+      children: [
+        Container(
+          height: 45,
+          margin: const EdgeInsets.only(left: 10, right: 10, top: 20),
+          child: TextField(
+            controller: searchQuery,
+            decoration: InputDecoration(
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(45),
+                borderSide: BorderSide(color: Colors.grey),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(45),
+                borderSide: BorderSide(color: Colors.grey),
+              ),
+              prefixIcon: Icon(Icons.search_rounded),
+              suffixIcon: Container(
+                decoration: BoxDecoration(
+                  color: Colors.grey,
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(25),
+                    bottomRight: Radius.circular(25),
+                  ),
+                ),
+                child: IconButton(
+                  onPressed: () {
+                    searchQuery.clear();
+                  },
+                  icon: Icon(Icons.cancel),
+                ),
+              ),
+              isDense: true,
+              hintText: 'search location, title, .....',
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
