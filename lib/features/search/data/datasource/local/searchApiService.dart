c3d484a373e7123e3fd24f64af355ea78ef66042
ad-ng <adolphengoga@gmail.com>
2025-05-19 19:31:06 +0200

    fetching categories

--- lib/features/search/data/datasource/local/searchApiService.dart
+++ lib/features/search/data/datasource/local/searchApiService.dart
@@ -1,6 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:quickchance_app/conf/dioservice.dart';
 import 'package:quickchance_app/features/home/data/models/opportunity_model.dart';
+import 'package:quickchance_app/features/search/data/models/categoryModel.dart';
 
 class SearchApiService {
   final Dio _dio = DioService.instance.dio;
@@ -15,6 +16,27 @@ class SearchApiService {
 
       if (dataJson != null && dataJson is List) {
         return dataJson.map((json) => OpportunityModel.fromJson(json)).toList();
+      } else {
+        throw Exception(
+          'Expected a list of properties but got ${dataJson.runtimeType}',
+        );
+      }
+    } on DioException catch (e) {
+      // Handle Dio errors
+      throw e.message!;
+    } catch (e) {
+      // Catch other errors
+      return Future.error('Something went wrong: $e');
+    }
+  }
+
+  Future<List<CategoryModel>> fetchAllCategories() async {
+    try {
+      final response = await _dio.get('/category');
+      final dataJson = response.data['data'];
+
+      if (dataJson != null && dataJson is List) {
+        return dataJson.map((json) => CategoryModel.fromJson(json)).toList();
       } else {
         throw Exception(
           'Expected a list of properties but got ${dataJson.runtimeType}',
