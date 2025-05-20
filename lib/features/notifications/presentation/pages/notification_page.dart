3e2cbac43a240fa2cf9b8116b71a888638bae4f5
ad-ng <adolphengoga@gmail.com>
2025-05-20 08:35:11 +0200

    updating notification page app bar

--- lib/features/notifications/presentation/pages/notification_page.dart
+++ lib/features/notifications/presentation/pages/notification_page.dart
@@ -10,6 +10,8 @@ class NotificationPage extends StatefulWidget {
 class _NotificationPageState extends State<NotificationPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: Text('Notifications'), centerTitle: true),
+    );
   }
 }
