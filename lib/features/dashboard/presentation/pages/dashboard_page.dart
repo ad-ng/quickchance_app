64fdcefb1d24197b597b8559096c3b0934dd5d6a
ad-ng <adolphengoga@gmail.com>
2025-05-24 07:49:26 +0200

    fixing admin dashbboard

--- lib/features/dashboard/presentation/pages/dashboard_page.dart
+++ lib/features/dashboard/presentation/pages/dashboard_page.dart
@@ -10,6 +10,50 @@ class DashboardPage extends StatefulWidget {
 class _DashboardPageState extends State<DashboardPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: Text('Dashboard'), centerTitle: true),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Card(
+              child: ListTile(
+                leading: Icon(Icons.people),
+                title: Text('100 users'),
+                subtitle: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('20 users'),
+                    Text('8 moderators'),
+                    Text('2 admins'),
+                  ],
+                ),
+                trailing: Icon(Icons.chevron_right_outlined),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 8, right: 8),
+            child: Card(
+              child: ListTile(
+                leading: Icon(Icons.post_add_rounded),
+                title: Text('100 posts'),
+                trailing: Icon(Icons.chevron_right_outlined),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 8, right: 8),
+            child: Card(
+              child: ListTile(
+                leading: Icon(Icons.category_rounded),
+                title: Text('9 categories'),
+                trailing: Icon(Icons.chevron_right_outlined),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
