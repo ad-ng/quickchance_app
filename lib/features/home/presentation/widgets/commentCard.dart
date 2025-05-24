d4ae50ce76386810b8da2cc6743572f951637235
ad-ng <adolphengoga@gmail.com>
2025-05-24 13:39:24 +0200

    updating comment card

--- lib/features/home/presentation/widgets/commentCard.dart
+++ lib/features/home/presentation/widgets/commentCard.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:popover/popover.dart';
 import 'package:quickchance_app/features/home/data/models/commentModel.dart';
 
 class CommentCard extends StatelessWidget {
@@ -29,7 +30,59 @@ class CommentCard extends StatelessWidget {
             ],
           ),
           isThreeLine: true,
-          trailing: Icon(Icons.more_vert_rounded),
+          trailing: Builder(
+            builder: (context) {
+              return GestureDetector(
+                onTap: () {
+                  showPopover(
+                    context: context,
+                    direction: PopoverDirection.left,
+                    bodyBuilder:
+                        (context) => Container(
+                          width: 120,
+                          height: 70,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    'Report',
+                                    style: TextStyle(
+                                      fontSize: 16,
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                  SizedBox(width: 8),
+                                  Icon(Icons.flag),
+                                ],
+                              ),
+                              SizedBox(height: 5),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    'Delete',
+                                    style: TextStyle(
+                                      fontSize: 16,
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                  SizedBox(width: 8),
+                                  Icon(Icons.delete),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                  );
+                },
+                child: Icon(Icons.more_vert_rounded),
+              );
+            },
+          ),
         ),
       ),
     );
