6e174fa737e29e5b527588edcbd948450a9f794c
ad-ng <adolphengoga@gmail.com>
2025-05-31 08:15:01 +0200

    fetching all opps

--- lib/features/dashboard/presentation/pages/dash_opp_page.dart
+++ lib/features/dashboard/presentation/pages/dash_opp_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:quickchance_app/features/home/presentation/bloc/opportunity_cubit.dart';
 
 class DashOppPage extends StatefulWidget {
   const DashOppPage({super.key});
@@ -10,6 +12,37 @@ class DashOppPage extends StatefulWidget {
 class _DashOppPageState extends State<DashOppPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(title: Text('Opportunities Page'), centerTitle: true),
+      body: Column(
+        children: [
+          Expanded(
+            child: BlocBuilder<OpportunityCubit, OpportunityState>(
+              builder: (context, state) {
+                if (state is OpportunityLoading) {
+                  return Center(child: CircularProgressIndicator.adaptive());
+                }
+                if (state is OpportunitySuccess) {
+                  return ListView.builder(
+                    addAutomaticKeepAlives: true,
+                    itemCount: state.response.length,
+                    itemBuilder:
+                        (context, index) => ListTile(
+                          title: Text(state.response[index].title),
+                          subtitle: Text(state.response[index].user.email!),
+                          trailing: IconButton(
+                            onPressed: () {},
+                            icon: Icon(Icons.more_vert_outlined),
+                          ),
+                        ),
+                  );
+                }
+                return SizedBox.expand();
+              },
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
