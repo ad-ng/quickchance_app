a56e42a2cc861540a28e2691844bd46f047aff69
ad-ng <adolphengoga@gmail.com>
2025-05-31 08:38:08 +0200

    adding search to to opp dashboard

--- lib/features/dashboard/presentation/pages/dash_opp_page.dart
+++ lib/features/dashboard/presentation/pages/dash_opp_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:quickchance_app/features/home/presentation/bloc/opportunity_cubit.dart';
+import 'package:quickchance_app/features/search/presentation/bloc/search_cubit.dart';
+import 'package:quickchance_app/features/search/presentation/widgets/mySearch.dart';
 
 class DashOppPage extends StatefulWidget {
   const DashOppPage({super.key});
@@ -10,19 +11,36 @@ class DashOppPage extends StatefulWidget {
 }
 
 class _DashOppPageState extends State<DashOppPage> {
+  TextEditingController searchQuery = TextEditingController();
+  @override
+  void initState() {
+    super.initState();
+    BlocProvider.of<SearchCubit>(context).searchOpp('');
+    searchQuery.addListener(() {
+      BlocProvider.of<SearchCubit>(context).searchOpp(searchQuery.text);
+    });
+  }
+
+  @override
+  void dispose() {
+    searchQuery.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Opportunities Page'), centerTitle: true),
       body: Column(
         children: [
+          MySearch(searchQuery: searchQuery),
           Expanded(
-            child: BlocBuilder<OpportunityCubit, OpportunityState>(
+            child: BlocBuilder<SearchCubit, SearchState>(
               builder: (context, state) {
-                if (state is OpportunityLoading) {
+                if (state is SearchLoading) {
                   return Center(child: CircularProgressIndicator.adaptive());
                 }
-                if (state is OpportunitySuccess) {
+                if (state is SearchSuccess) {
                   return ListView.builder(
                     addAutomaticKeepAlives: true,
                     itemCount: state.response.length,
