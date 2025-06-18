b23ccab45547a9ee3006d0c106b26d38b8d471c2
ad-ng <adolphengoga@gmail.com>
2025-06-18 08:23:31 +0200

    finishing up preferences

--- lib/features/profile/presentation/bloc/preferencescubit.dart
+++ lib/features/profile/presentation/bloc/preferencescubit.dart
@@ -1,6 +1,8 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:quickchance_app/features/profile/data/model/preference_model.dart';
 import 'package:quickchance_app/features/profile/domain/repository/profile_repo.dart';
+import 'package:quickchance_app/features/search/data/datasource/local/searchApiService.dart';
+import 'package:quickchance_app/features/search/data/models/categoryModel.dart';
 
 class PreferencesCubit extends Cubit<PreferencesState> {
   final ProfileRepo profileRepo;
@@ -11,8 +13,21 @@ class PreferencesCubit extends Cubit<PreferencesState> {
   Future getPreferences() async {
     emit(PreferencesLoading());
     try {
-      final response = await profileRepo.fetchPreferences();
-      emit(PreferencesSuccess(response));
+      final preferences =
+          await profileRepo.fetchPreferences(); // already selected
+      final allCategories =
+          await SearchApiService().fetchAllCategories(); // all available
+
+      // Extract selected category IDs from preferences
+      final selectedCategoryIds = preferences.map((e) => e.categoryId).toSet();
+
+      // Filter out categories already in preferences
+      final unselectedCategories =
+          allCategories
+              .where((category) => !selectedCategoryIds.contains(category.id))
+              .toList();
+
+      emit(PreferencesSuccess(preferences, unselectedCategories));
     } catch (e) {
       emit(PreferencesError(e.toString()));
     }
@@ -25,7 +40,8 @@ class InitialPreference extends PreferencesState {}
 
 class PreferencesSuccess extends PreferencesState {
   final List<PreferenceModel> response;
-  PreferencesSuccess(this.response);
+  final List<CategoryModel> results;
+  PreferencesSuccess(this.response, this.results);
 }
 
 class PreferencesLoading extends PreferencesState {}
